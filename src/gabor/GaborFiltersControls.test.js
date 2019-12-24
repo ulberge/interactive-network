015d01c69f864aaf83403a54c74af48790f150f0
Erik Ulberg <ulberge@gmail.com>
2019-12-24 09:57:40 -0500

    fix test cruft

--- src/gabor/GaborFiltersControls.test.js
+++ src/gabor/GaborFiltersControls.test.js
@@ -2,19 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GaborFiltersControls from './GaborFiltersControls';
 
-const filterConfig = {
-  numComponents: 3,
-  lambda: 4,
-  gamma: 2,
-  sigma: 1.1,
-  windowSize: 5
-};
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const filterConfig = {
-
-  }
   ReactDOM.render(
     <GaborFiltersControls
       numComponents={3}
