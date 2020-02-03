7c0cdafc089a0fe80cd6908daf168da868e4decb
Erik Ulberg <ulberge@gmail.com>
2020-02-03 14:42:50 -0500

    reorg folders for deployment of demo

---
+++ docs/precache-manifest.1c8741e8b1cf8eec34ecd980a5e5f8d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a371df3673396a12bc81d1e6993429d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf22897e8a8b675fd95",
+    "url": "/static/css/main.15be6fa4.chunk.css"
+  },
+  {
+    "revision": "e76bc833cd90500b17ce",
+    "url": "/static/js/2.a2c06d32.chunk.js"
+  },
+  {
+    "revision": "caf6895a30b76d54e3dc1df0e491c250",
+    "url": "/static/js/2.a2c06d32.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcf22897e8a8b675fd95",
+    "url": "/static/js/main.b3226ffc.chunk.js"
+  },
+  {
+    "revision": "9be79bb2e379fc44ec0c",
+    "url": "/static/js/runtime-main.6e50c70c.js"
+  }
+]);
